3ac2164fd1fd6d46488bdd574d41bd9ee7d049c7
Sgun369 <souhaiebjebali@gmail.com>
2024-04-10 17:17:10 -0400

    processes

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,19 +1,2 @@
 #!/bin/bash
-
-# check if a user was provided as an argument
-if [ -z "$1" ]; then
-
-  echo "Usage: $0 <username>"
-
-  exit 1
-
-fi
-
-ps -U "$1" -o pid,user,%mem,%cpu,vsz,rss | awk '
-BEGIN {FS=" "}
-{
-    if ($5 > 0 && 6 >0) {
-        printf "%-5s %-8s %-4s %-4s %-6s %-6s\n", $1, $2, $3, $4, $5, $6
-    }
-}
-'
+ps -U "$1" -o pid,user,%mem,%cpu,vsz,rss --no-headers | awk '$5 > 0 && 6 > 0 {print $0}' 
