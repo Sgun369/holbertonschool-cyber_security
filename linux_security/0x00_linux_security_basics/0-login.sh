6ae572db083b89e337e00f8cb15176faed723600
Sgun369 <souhaiebjebali@gmail.com>
2024-04-16 09:22:18 -0400

    what secrets hold

--- linux_security/0x00_linux_security_basics/0-login.sh
+++ linux_security/0x00_linux_security_basics/0-login.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-[[ $EUID -ne 0 ]] && { echo "This sript must be run as root or sudo" 1>&2; exit 1; }
-last -n 5 | awk '{print $1,$5,$6,$7,$8,$9}'
+[[ $EUID -ne 0 ]] && last -n 5 | awk '{print $1,$5,$6,$7,$8,$9}'
