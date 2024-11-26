d7a9fee5607b57a572baf72d3e821b75873f9087
Sgun369 <souhaiebjebali@gmail.com>
2024-11-26 12:45:06 -0500

    0-whois

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk '/Registrant|Admin|Tech/ {print $1 "," $2 "," $3 "," $4}'
+whois "$1" | awk -F': ' '/^(Registrant|Admin|Tech) / {print $1 "," $2}' | sed 's/State\/Province/State\/Province/' > "$1.csv"
