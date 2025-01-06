88e7a728442d87223c10096927d442accaf2b7f5
Sgun369 <souhaiebjebali@gmail.com>
2025-01-06 14:26:45 -0500

    monor fix

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-md5 "$1" && john --show "$1" | grep -v -e '^$' | awk -F: '{print $2}' >> 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-md5 "$1"
--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=NT "$1" && john --show "$1" | grep -v '^$' | awk -F: '{print $2}' >> 5-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=NT "$1"
