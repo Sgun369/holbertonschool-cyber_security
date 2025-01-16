ae3cf49faf408ed232be0cc3551b7270a4f3d1ff
Sgun369 <souhaiebjebali@gmail.com>
2025-01-16 04:13:58 -0500

    2

--- linux_security/0x03_protocols_servers/2-harden.sh
+++ linux_security/0x03_protocols_servers/2-harden.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find / -type d -perm -0002 -xdev ! -path "/proc/*" ! -path "/sys*" ! -path "/dev/*" -exec chmod o-w {}
+find / -type d -perm -0002 -xdev ! -path "/proc/*" ! -path "/sys*" ! -path "/dev/*" -exec chmod o-w {} +
