445557bbe58b5bb07c6d8526dac0bbf97dee252e
Sgun369 <souhaiebjebali@gmail.com>
2025-01-27 07:56:30 -0500

    0

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-
-LOG_FILE="logs.txt"
-
-if [[ ! -f "$LOG_FILE" ]]; then
-    echo "Log file $LOG_FILE does not exist!"
-    exit 1
-fi
-MOST_REQUESTS_IP=$(awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 1)
-
-echo "IP address with the most requests: $MOST_REQUESTS_IP"
-
+grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}' 
